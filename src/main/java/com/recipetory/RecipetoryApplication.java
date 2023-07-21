fb8bbd681a09526d6adc25041bbb75c7a2223118
buchuKim <qkqdlek32@gmail.com>
2023-07-21 17:10:09 +0900

    initial commit

---
+++ src/main/java/com/recipetory/RecipetoryApplication.java
@@ -0,0 +1,13 @@
+package com.recipetory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipetoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipetoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipetory/RecipetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipetory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
