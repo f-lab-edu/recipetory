93622727d0971121884f74bc17df3ac07f17cf60
buchu <qkqdlek32@gmail.com>
2023-08-14 21:50:12 +0900

    fix : TagDto, Follow Entity 클래스 잡다한 것 수정

--- src/main/java/com/recipetory/user/domain/follow/Follow.java
+++ src/main/java/com/recipetory/user/domain/follow/Follow.java
@@ -3,12 +3,16 @@ package com.recipetory.user.domain.follow;
 import com.recipetory.user.domain.User;
 import com.recipetory.utils.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Follow extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
