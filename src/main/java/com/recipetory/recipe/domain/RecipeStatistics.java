89b1f589a65f2130f63f6f8093fbab2f5f280648
buchu <qkqdlek32@gmail.com>
2023-09-21 00:23:47 +0900

    fix : 사용하지 않는 `import` 제거

--- src/main/java/com/recipetory/recipe/domain/RecipeStatistics.java
+++ src/main/java/com/recipetory/recipe/domain/RecipeStatistics.java
@@ -1,6 +1,5 @@
 package com.recipetory.recipe.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
